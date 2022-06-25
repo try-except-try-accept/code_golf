8b87a849b4bc0fdcae25df4c9cac239855ac7b6a
try-except-try-accept <chall168421@gmail.com>
2022-06-25 08:22:43 +0700

    352nd place

--- xmas_trees.py
+++ xmas_trees.py
@@ -1,4 +1,14 @@
+
+# attempt 1 - 407th
 t=["   *","  ***"," *****","   *"]
 for i in range(7):
     print("\n".join(t)+"\n")
     (t := [" "+j for j in t]).insert(-1," *"+t[-2].strip()+"*")
+
+# attempt 2 - 352nd
+r,p=range,print
+for i in r(3,10):
+    for j in r(i):
+        p((" "*(i-j))+("*"*((j*2)+1)))
+    p((" "*i)+"*\n")
+
