d6361914b995707d8070bc0b5abaec35ba0f91dc
try-except-try-accept <chall168421@gmail.com>
2022-06-25 13:51:04 +0700

    52nd in bytes! 🖖

--- rock_paper_scissors_lizard_spock.py
+++ rock_paper_scissors_lizard_spock.py
@@ -1,36 +1,4 @@
-import sys
-
-class sys:
-    argv = [None] + '''🦎🖖
-🦎💎
-💎💎
-🖖🦎
-📄💎
-🦎✂
-✂💎
-📄✂
-💎🦎
-🖖💎
-💎🖖
-📄🦎
-🖖📄
-✂🖖
-✂🦎
-💎📄
-🖖🖖
-✂✂
-🦎🦎
-📄📄
-🖖✂
-💎✂
-✂📄
-📄🖖
-🦎📄'''.split("\n")
-
-import re
+import sys, re
 w="'✂ cuts 📄 covers 💎 crushes 🦎 poisons 🖖 smashes ✂ decapitates 🦎 eats 📄 disproves 🖖 vaporizes 💎 crushes ✂'"
 for a,b in sys.argv[1:]:  
-    print("Tie" if a==b else re.findall(f"({a}|{b})\s\w+\s({a}|{b})",w)[0])
-
-    
-    
+    print("Tie" if a==b else re.search(f"({a}|{b})\s\w+\s({a}|{b})",w).group())
