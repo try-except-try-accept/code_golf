89e7ba7fe09da83d86bd2f38f1102881c20debc4
chris <chall168421@gmail.com>
2022-06-27 11:13:26 +0700

    code as unicode finished!

--- encode_python_as_unicode_idea.py
+++ encode_python_as_unicode_idea.py
@@ -1,10 +1,23 @@
 code="print('hello world')"
+#convert each char in code to 2 hex digits
 code_hex=[hex(ord(i))[2:].zfill(2) for i in code]
 for i in code_hex:
     print("i is", i, y:=int(i, 16), chr(y))
-x = [code_hex[j:j+5] for j in range(0,len(code_hex),5)]
-s="".join(hex(ord(i))[2:]for i in x)
-s=[s[i:i+2] for i in range(0, len(s), 2)]
+# concat hex string 
+hex_2_string = "".join(code_hex)
+print(hex_2_string)
+# convert each 5 hex chars into unicode
+unicode = [chr(int(hex_2_string[j:j+5],16)) for j in range(0,len(hex_2_string),5)]
+
+print(unicode)
+input()
+# unicode back into hex
+unicode_2_hex = "".join([hex(ord(i))[2:].zfill(5) for i in unicode])
+print(unicode_2_hex)
+# unicode hex back to 2-digit hex codes
+s=[unicode_2_hex[i:i+2].zfill(2) for i in range(0, len(unicode_2_hex), 2)]
+print(s)
+input()
 with open("run.py", "w") as f:
     for i in s:
         print("i is", i, y:=int(i, 16), chr(y))
