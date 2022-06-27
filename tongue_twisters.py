69d90d1d9370a8b13a4b2cae06f7cc499ebe8373
chris <chall168421@gmail.com>
2022-06-27 13:57:24 +0700

    unicode encoded solution works.. 50th place!

---
+++ tongue_twisters.py
@@ -0,0 +1,3 @@
+a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,q,p,r,s,t="wood/would/chuck/could/Peter Piper/picked/peck/pickled peppers/she/sells/sea/shells/shore/,\n/.\n/?\n/If a/I'm sure/of/the".split("/")
+x,z,w=a+c,e+" "+f,g+" "+s
+print(" ".join(["How much",a,b,"a",x,c+n+p,x,d,c,a+q+"A",x,b,c,"all",t,a,"he",d,c+"\n"+p,x,b,c,a+o+"\n"+z,"a",w,h+o+"A",w,h,z+o+"If",z,"a",w,h+n+"Where's",t,w,h,z+q+"\n"+"She",j,k+l,"by",t,k+m+n+"The",l,i,j,"are",k+l+",",r+o+"So if",i,j,k+l,"on",t,k+m+n+"Then",r,i,j,k+m,l+"."]))
